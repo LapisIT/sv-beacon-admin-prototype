eecf89724d31465207996c87c35319a72e156045
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2016-04-04 09:07:22 +0800

    initial signal display done

--- app/features/attendees/attendees.js
+++ app/features/attendees/attendees.js
@@ -11,9 +11,12 @@ angular.module('svBeaconAdminPrototypeApp')
   .controller('AttendeesCtrl', function ($log, $scope, Events, Validations) {
     var ctrl = this, isDefined = Validations.isDefined;
 
-    ctrl.create = function() {
-
+    ctrl.eventInfo = function(signal) {
+      var beacon = signal.beacon;
+      return ctrl.event.beacons[Events.beacons.toKey(beacon.uuid,beacon.major,beacon.minor)]['locationName']
     }
+
+
 
     Events.load().then(function (event) {
       $log.info('Events.load() ', event);
